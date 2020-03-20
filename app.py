3ce9eca31b9a03fa90fb68e5c300ea572520b50b
cccs-cat001 <cccs-cat001@cyber.gc.ca>
2020-03-20 10:49:24 -0400

    3.9

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 stores = [
@@ -12,19 +12,30 @@ stores = [
         ]
     }
 ]
+
+
 @app.route('/')
 def home():
-    return "Hello world!"
+    return render_template('index.html')
 
 
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 
 @app.route('/store/<string:name>', methods=['GET'])
 def get_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
 
 
 @app.route('/store', methods=['GET'])
@@ -34,12 +45,24 @@ def get_stores():
 
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-    pass
+    request_data = request.get_json
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message': 'store not found'})
 
 
 @app.route('/store/<string:name>/item', methods=['GET'])
 def get_items_in_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store['items'])
+    return jsonify({'message': 'no store'})
 
 
 app.run(port=5000)
