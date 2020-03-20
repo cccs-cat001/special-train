ed922891a31b52de3a314d5cf2162e1ff89cbe4d
cccs-cat001 <cccs-cat001@cyber.gc.ca>
2020-03-20 13:32:40 -0400

    4.13

--- ch4/code/app.py
+++ ch4/code/app.py
@@ -14,6 +14,12 @@ items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank"
+    )
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -23,20 +29,14 @@ class Item(Resource):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': 'an item with name {} already exists'.format(name)}, 400
 
-        request_data = request.get_json(force=True)
-        item = {'name': name, 'price': request_data['price']}
+        data = Item.parser.parse_args()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
     def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument('price', 
-            type=float, 
-            required=True,
-            help="This field cannot be left blank"
-        )
-        data = parser.parse_args()
-        
+        data = Item.parser.parse_args()
+
         item = next(filter(lambda x: x['name'] == name, items), None)
         if not item:
             item = {'name': data['name'], 'price': data['price']}
