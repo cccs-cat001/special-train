d271ee70e688cfb839c5ef06923738623c3f5b82
cccs-cat001 <cccs-cat001@cyber.gc.ca>
2020-03-20 13:28:24 -0400

    4.12

--- ch4/code/app.py
+++ ch4/code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -29,7 +29,14 @@ class Item(Resource):
         return item, 201
 
     def put(self, name):
-        data = request.get_json()
+        parser = reqparse.RequestParser()
+        parser.add_argument('price', 
+            type=float, 
+            required=True,
+            help="This field cannot be left blank"
+        )
+        data = parser.parse_args()
+        
         item = next(filter(lambda x: x['name'] == name, items), None)
         if not item:
             item = {'name': data['name'], 'price': data['price']}
@@ -42,6 +49,7 @@ class Item(Resource):
         global items
         items = list(filter(lambda x: x['name'] != name, items))
         return {'message': 'item deleted'}
+
 
 class ItemList(Resource):
     def get(self):
