5fe984f1daffd8733b3936d06108ce37c6df3ffb
cccs-cat001 <cccs-cat001@cyber.gc.ca>
2020-03-20 10:27:21 -0400

    3.7

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 stores = [
@@ -12,29 +12,34 @@ stores = [
         ]
     }
 ]
-@app.route('/') 
-def home(): 
+@app.route('/')
+def home():
     return "Hello world!"
+
 
 @app.route('/store', methods=['POST'])
 def create_store():
     pass
 
+
 @app.route('/store/<string:name>', methods=['GET'])
 def get_store(name):
     pass
 
+
 @app.route('/store', methods=['GET'])
 def get_stores():
-    pass
+    return jsonify({'stores': stores})
+
 
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
     pass
 
+
 @app.route('/store/<string:name>/item', methods=['GET'])
 def get_items_in_store(name):
     pass
 
-app.run(port=5000)
 
+app.run(port=5000)
