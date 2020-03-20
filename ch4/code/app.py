742ada010fd57b677893f99dad8c852dfdbfcbf9
cccs-cat001 <cccs-cat001@cyber.gc.ca>
2020-03-20 12:37:16 -0400

    4.6

--- ch4/code/app.py
+++ ch4/code/app.py
@@ -1,13 +1,36 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
+items = []
+
+
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
 
-api.add_resource(Student, '/student/<string:name>')
+    def post(self, name):
+        request_data = request.get_json(force=True)
+        item = {'name': name, 'price': request_data['price']}
+        items.append(item)
+        return item, 201
 
-app.run()
+    def put(self, name):
+        pass
+
+    def delete(self, name):
+        pass
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+app.run(debug=True)
