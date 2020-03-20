cbda42c7e9faf91db6c5d810ac3823f5557a6bfb
cccs-cat001 <cccs-cat001@cyber.gc.ca>
2020-03-20 13:09:43 -0400

    4.9

--- ch4/code/app.py
+++ ch4/code/app.py
@@ -1,17 +1,23 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = "1234"
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity)
+
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        item = next(filter(lambda x: x['name']==name, items), None)
-        return {'item': None}, 200 if item else 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None):
@@ -32,6 +38,7 @@ class Item(Resource):
 class ItemList(Resource):
     def get(self):
         return {'items': items}
+
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
