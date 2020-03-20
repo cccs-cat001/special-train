9d094d525d3f1e9010ebb813f119ba54737f36f2
cccs-cat001 <cccs-cat001@cyber.gc.ca>
2020-03-20 09:57:15 -0400

    3.6

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+stores = [
+    {
+        'name': 'store1',
+        'items': [
+            {
+                'name': 'item1',
+                'price': 10.99
+            }
+        ]
+    }
+]
 @app.route('/') 
 def home(): 
     return "Hello world!"
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+@app.route('/store/<string:name>', methods=['GET'])
+def get_store(name):
+    pass
+
+@app.route('/store', methods=['GET'])
+def get_stores():
+    pass
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+@app.route('/store/<string:name>/item', methods=['GET'])
+def get_items_in_store(name):
+    pass
 
 app.run(port=5000)
 
