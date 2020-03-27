bdcfd34271c6ee1a07225ed3f809bf75475c2950
cccs-cat001 <cccs-cat001@cyber.gc.ca>
2020-03-27 11:41:06 -0400

    6.9

--- ch6/code/resources/item.py
+++ ch6/code/resources/item.py
@@ -9,6 +9,11 @@ class Item(Resource):
                         required=True,
                         help="This field cannot be left blank"
                         )
+    parser.add_argument('store_idint',
+                        type=float,
+                        required=True,
+                        help="Every item needs a store id"
+                        )
 
     @jwt_required()
     def get(self, name):
@@ -22,7 +27,7 @@ class Item(Resource):
             return {'message': 'an item with name {} already exists'.format(name)}, 400
 
         data = Item.parser.parse_args()
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, **data)
         try:
             item.save_to_db()
         except:
@@ -36,7 +41,7 @@ class Item(Resource):
         item = ItemModel.find_by_name(name)
 
         if not item:
-            item = ItemModel(name, data['price'])
+            item = ItemModel(name, **data)
 
         else:
             item.price = data['price']
