60e5c035a3440babb007732cd783caaa8d163e6e
cccs-cat001 <cccs-cat001@cyber.gc.ca>
2020-03-27 11:25:30 -0400

    6.8

--- ch6/code/app.py
+++ ch6/code/app.py
@@ -12,6 +12,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.secret_key = "1234"
 api = Api(app)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+    
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>')
