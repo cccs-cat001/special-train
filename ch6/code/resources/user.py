fb64679f3898faca8160178d0d0f15d78e695b2f
cccs-cat001 <cccs-cat001@cyber.gc.ca>
2020-03-27 11:16:51 -0400

    6.6

--- ch6/code/resources/user.py
+++ ch6/code/resources/user.py
@@ -11,15 +11,9 @@ class UserRegister(Resource):
         data = UserRegister.parser.parse_args()
         
         if UserModel.find_by_username(data['username']):
-            return {'message': 'User {} already exists'.format(data['username'])}
+            return {'message': 'User \'{}\' already exists'.format(data['username'])}
 
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        cursor.execute(query, (data['username'], data['password']))
-
-        connection.commit()
-        connection.close()
+        user = UserModel(**data)
+        user.save_to_db()
 
         return {'message': 'User created successfully'}, 201
