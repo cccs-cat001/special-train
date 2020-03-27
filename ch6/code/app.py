63279ee55e8705c58a779f14d0f962d7f55687a4
cccs-cat001 <cccs-cat001@cyber.gc.ca>
2020-03-27 10:40:25 -0400

    6.4

--- ch6/code/app.py
+++ ch6/code/app.py
@@ -7,6 +7,7 @@ from security import authenticate, identity
 from resources.user import UserRegister
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "1234"
 api = Api(app)
 
@@ -16,4 +17,6 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
     app.run(debug=True)
--- ch6/code/models/item.py
+++ ch6/code/models/item.py
@@ -1,6 +1,12 @@
 import sqlite3
+from db import db
+class ItemModel(db.model):
+    __tablename__ = 'items'
 
-class ItemModel:
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    price = db.Column(db.Fload(precision=2))
+    
     def __init__(self, name, price):
         self.name = name
         self.price = price
