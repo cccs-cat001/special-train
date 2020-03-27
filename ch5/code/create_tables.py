79c392e28ce3f207e31272ce4400296a91049bb6
cccs-cat001 <cccs-cat001@cyber.gc.ca>
2020-03-27 08:42:08 -0400

    5.7

--- ch5/code/create_tables.py
+++ ch5/code/create_tables.py
@@ -7,7 +7,7 @@ cursor.execute(create_table)
 
 create_table = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
 cursor.execute(create_table)
-cursor.execute("INSERT INTO items VALUES ('test', 9.99)")
+
 connection.commit()
 
 connection.close()
